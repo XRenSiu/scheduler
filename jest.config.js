290f93fd6c34138e9c751882e8e08d3c20dff540
XRenSiu <xrensiu@gmail.com>
2020-06-29 18:11:07 +0800

    add jest coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: __dirname,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'lcov', 'text'],
+  collectCoverageFrom: ['src/**/*.ts'],
   testMatch: ['<rootDir>/src/__tests__/**/*spec.[jt]s?(x)']
 }
